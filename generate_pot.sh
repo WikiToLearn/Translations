33b56aafe323c97607244a80a7ffb769f3e010d3
Riccardo Iaconelli <riccardo@kde.org>
2016-09-06 00:02:03 +0200

    add courseeditor json
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- generate_pot.sh
+++ generate_pot.sh
@@ -13,4 +13,19 @@ for L in $LANGS; do
 
   # Generate ini files
   po2ini -t ini po-$L $L
+
+done
+
+for L in $LANGS; do
+
+  mkdir -p $L po-$L
+  # Extract strings
+  json2po -P json templates
+
+  # Update translations
+  pot2po -t po-$L/ templates/ po-$L/
+
+  # Generate ini files
+  po2json -t json po-$L $L
+
 done
