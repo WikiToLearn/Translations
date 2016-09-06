f190bd0551f5496b7fa83f1ce94d1feb57ef91ad
Riccardo Iaconelli <riccardo@kde.org>
2016-09-06 11:01:36 +0200

    default mediawiki to english where strings are not there
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- compile.py
+++ compile.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python
+
 import configparser, os
 
 languages = ["it", "en", "fr", "es"]
 
+config = configparser.ConfigParser()
+
+enconfig = configparser.ConfigParser()
+enconfig.readfp(open("wtl-messages/en.ini",'r'))
+
 for f in languages:
-    config = configparser.ConfigParser()
-    config.readfp(open(f+"/wtl-messages.ini"))
+    config.readfp(open("wtl-messages/"+f+".ini",'r'))
     outarr = []
     for identifier, string in config.items("messages"):
+        if len(string) == 0:
+           string = enconfig.get("messages", identifier)
+
         out = "<section begin=" + identifier
         out += " />" + string
         out += "<section end=" + identifier +  " /> "
