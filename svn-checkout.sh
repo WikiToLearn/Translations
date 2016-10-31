fe7137ea8920c4e1f3dd06b95037293f910a873e
Toma Luca <toma.luca95@gmail.com>
2016-10-31 13:36:45 +0100

    updated svn-checkout.sh (svn clean copy everytime)
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- svn-checkout.sh
+++ svn-checkout.sh
@@ -12,18 +12,6 @@ cd locales/
 for lang in it de es fr pt sv ca
 do
     echo "Update for "$lang"..."
-    FORCE_SVN_NEW=1
-    if cd $lang
-    then
-        svn update
-        cd ..
-    else
-        FORCE_SVN_NEW=0
-    fi
-
-    if [[ $FORCE_SVN_NEW -eq 0 ]]
-    then
-        rm $lang -Rf
-        svn co svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/$lang/messages/wikitolearn $lang
-    fi
+    rm -Rf $lang
+    svn co svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/$lang/messages/wikitolearn $lang
 done
