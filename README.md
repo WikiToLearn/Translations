0552ec0f1002e00f9d4d659a7c59e9e84161a413
Toma Luca <toma.luca95@gmail.com>
2017-02-11 17:18:27 +0100

    added a woking tool for Tool-WikiPages-en, need debugging (or carefull real world usage)
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## This is a WIP for a new Translation system
 
-To use this you have to install python3 and `pip`.
+To use this you have to install python3, `pip` and `translate-toolkit`.
 
-You have to install `pip install -r requirements.txt`
-
-After the `./pot-gen.py` to create actual pot files in the `./pot/` directory
+You have to install `pip install -r requirements.txt`.
---
+++ Tool-WikiPages-en/common.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+mypath = "{}/".format(os.path.dirname(os.path.realpath(__file__)))
+
+work_dir = "{}workdir/".format(mypath)
+if not os.path.exists(work_dir):
+    os.makedirs(work_dir)
+
+output_pot_dir = "{}pot/".format(work_dir)
+if not os.path.exists(output_pot_dir):
+    os.makedirs(output_pot_dir)
+
+output_mw_dir = "{}mw/".format(work_dir)
+if not os.path.exists(output_mw_dir):
+    os.makedirs(output_mw_dir)
+
+tmp_dir = "{}tmp/".format(work_dir)
+if not os.path.exists(tmp_dir):
+    os.makedirs(tmp_dir)
+
+tmp_output_po_dir = "{}po/".format(tmp_dir)
+if not os.path.exists(tmp_output_po_dir):
+    os.makedirs(tmp_output_po_dir)
+
+tmp_git_dir = "{}git/".format(tmp_dir)
+if not os.path.exists(tmp_git_dir):
+    os.makedirs(tmp_git_dir)
+
+tmp_mw_files = "{}mw-in-files/".format(tmp_dir)
+if not os.path.exists(tmp_mw_files):
+    os.makedirs(tmp_mw_files)
+
+git_repo = "https://github.com/WikiToLearn/WikiPages-en"
