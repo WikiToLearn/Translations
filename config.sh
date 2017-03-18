50c486ec2b0841d579fca216259b449f1ff7dd1d
Riccardo Iaconelli <riccardo@kde.org>
2017-03-18 11:27:07 +0100

    add a separate file for common configuration
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ config.sh
@@ -0,0 +1,11 @@
+# List of directories with en.json to translate
+JSON="courseeditor skin"
+# List of directories with ini files to translate
+INI="wtl-messages guide"
+
+TXT="guide"
+
+LANGS="ca fr es de it pt sv"
+
+#SVN_BASE="svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5"
+SVN_BASE="svn+ssh://svn@svn.kde.org/home/kde/trunk/l10n-kf5"
